19bb4106f395bcb7462e8a0ef1ab950bd9cd0325
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2022-09-22 18:49:26 -0400

    Add judge express route

--- src/backend/src/app.js
+++ src/backend/src/app.js
@@ -1,11 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 
+const judge = require('./routes/judge')
+
 const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json()) //http://expressjs.com/en/api.html#express.json
 app.use(express.urlencoded({ extended: false })) //http://expressjs.com/en/5x/api.html#express.urlencoded
+
+app.use('/judge', judge)
 
 app.listen(PORT, () => {
   console.info(`App listening on port ${PORT}`)
