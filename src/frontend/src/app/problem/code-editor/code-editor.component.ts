2dcd5bb98e5fc35f473b02657b195f7fc11dd378
Anton Kanugalawattage <antondilon@gmail.com>
2022-11-11 14:39:11 -0500

    [FE][CE] Implement code editor and ui elements

--- src/frontend/src/app/problem/code-editor/code-editor.component.ts
+++ src/frontend/src/app/problem/code-editor/code-editor.component.ts
@@ -1,8 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CodeModel } from '@ngstack/code-editor';
 
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
   styleUrls: ['./code-editor.component.scss'],
 })
-export class CodeEditorComponent {}
+export class CodeEditorComponent {
+  theme = 'vs-dark';
+
+  codeModel: CodeModel = {
+    language: 'typescript',
+    uri: 'solution.ts',
+    value: [`export class MyClass {`, `  constructor() {`, '', '  }', `}`].join(
+      '\n'
+    ),
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true,
+    },
+  };
+
+  onCodeChanged(value: any) {
+    console.log('CODE', value);
+  }
+}
