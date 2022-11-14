63274ec3b58bc581040e2caad78074c1dfec8323
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2022-11-14 14:13:22 -0500

    Add swagger docs to judge endpoint

--- src/backend/src/routes/judge.js
+++ src/backend/src/routes/judge.js
@@ -5,13 +5,48 @@ import judge from '../services/judge/index.js';
 
 const router = Router();
 
-router.post('/:id', async (req, res) => {
-  const { id } = req.params;
+/**
+ * @swagger
+ * /judge/{_id}:
+ *   post:
+ *     tags:
+ *        - judge
+ *     summary: Judges code against a problem's test cases
+ *     description: Judges code against a problem's test cases
+ *     parameters:
+ *       - name: _id
+ *         in: path
+ *         description: Problem id to judge
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - code
+ *               - language
+ *             properties:
+ *               code:
+ *                 type: string
+ *               language:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: If code passes all cases then All test cases passed! otherwise A test case failed.
+ *       '500':
+ *         description: Internal Failure
+ *
+ */
+router.post('/:_id', async (req, res) => {
+  const { _id } = req.params;
   const { code, language } = req.body;
   try {
-    const problem = await Problems.findById(id);
+    const problem = await Problems.findById(_id);
     if (!problem) {
-      res.status(404).send({ message: `Problem ${id} not found.` });
+      res.status(404).send({ message: `Problem ${_id} not found.` });
     }
     // eslint-disable-next-line no-restricted-syntax
     for await (const { input, output } of problem.test_cases) {
