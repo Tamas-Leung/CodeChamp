d03049abfa1229fcd481d7a4316bb7552d4707e8
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2022-11-14 13:52:29 -0500

    Add path and command generators for JavaScript

---
+++ src/backend/src/services/judge/language-generators.js
@@ -0,0 +1,15 @@
+import { v4 } from 'uuid';
+import path from 'path';
+
+export const JUDGE_DIR = 'temp-judge';
+const FILE_START = 'TEMP-CODE-';
+const PATH_START = path.join(JUDGE_DIR, FILE_START);
+
+const commands = Object.freeze({
+  js: `node `,
+});
+
+export const generatePath = (language) => `${PATH_START + v4()}.${language}`;
+
+export const generateCommand = (language, filePath) =>
+  commands[language] + filePath;
