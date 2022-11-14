1e635ed5a90a2aa83621a8068f35b734f92d65f3
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2022-11-14 13:57:45 -0500

    Add judge endpoint

--- src/backend/src/routes/judge.js
+++ src/backend/src/routes/judge.js
@@ -1,9 +1,38 @@
 import { Router } from 'express';
+import { Problems } from '../database/mongoose.js';
+
+import judge from '../services/judge/index.js';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-  res.send('GET /judge works');
+router.post('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { code, language } = req.body;
+  try {
+    const problem = await Problems.findById(id);
+    if (!problem) {
+      res.status(404).send({ message: `Problem ${id} not found.` });
+    }
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const { input, output } of problem.test_cases) {
+      const judgeResult = await judge({
+        language,
+        code,
+        input,
+        output,
+        timeout: problem.time_limit,
+      });
+      if (!judgeResult) {
+        res.status(200).send('A test case failed.');
+        return;
+      }
+    }
+    res.status(200).send('All test cases passed!');
+  } catch (e) {
+    res.status(500).send({
+      message: e,
+    });
+  }
 });
 
 export default router;
