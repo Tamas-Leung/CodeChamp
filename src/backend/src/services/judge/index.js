2b982cbb55e07c31f3a318ed9fa4f93d314d3df5
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2022-11-14 13:56:33 -0500

    Ensure dir exists before crreating judge files

--- src/backend/src/services/judge/index.js
+++ src/backend/src/services/judge/index.js
@@ -1,44 +1,67 @@
 import { exec } from 'child_process';
-import { writeFile, unlink } from 'fs';
+import { writeFile, unlink, access, constants, mkdir } from 'fs';
 
-import { generateCommand, generatePath } from './language-generators.js';
+import {
+  JUDGE_DIR,
+  generateCommand,
+  generatePath,
+} from './language-generators.js';
+
+const ensureJudgeDirExists = () =>
+  new Promise((resolve, reject) => {
+    access(JUDGE_DIR, constants.F_OK, (accessFailed) => {
+      if (accessFailed) {
+        mkdir(JUDGE_DIR, (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      }
+      resolve();
+    });
+  });
 
 export default ({ language, code, input, output, timeout }) =>
   new Promise((resolve, reject) => {
-    const codePath = generatePath(language);
-    writeFile(codePath, code, (err) => {
-      if (err) {
-        reject(err);
-      }
-    });
+    ensureJudgeDirExists()
+      .then(() => {
+        const codePath = generatePath(language);
+        writeFile(codePath, code, (err) => {
+          if (err) {
+            reject(err);
+          }
+        });
 
-    const cmd = generateCommand(language, codePath);
-    if (!cmd) {
-      reject(new Error(`Unsupported Language: ${language}`));
-    }
+        const cmd = generateCommand(language, codePath);
+        if (!cmd) {
+          reject(new Error(`Unsupported Language: ${language}`));
+        }
 
-    const child = exec(cmd, { timeout }, (err, stdout, stderr) => {
-      if (stderr) {
-        reject(stderr);
-      }
-      if (err) {
-        reject(err);
-      }
-      if (stdout) {
-        resolve(stdout === output);
-      }
-    });
+        const child = exec(cmd, { timeout }, (err, stdout, stderr) => {
+          if (stderr) {
+            reject(stderr);
+          }
+          if (err) {
+            reject(err);
+          }
+          if (stdout) {
+            resolve(stdout === output);
+          }
+        });
 
-    child.stdout.on('data', (stdout) => {
-      unlink(codePath, (err) => {
-        reject(err);
-      });
-      resolve(stdout === output);
-    });
+        child.stdout.on('data', (stdout) => {
+          unlink(codePath, (err) => {
+            reject(err);
+          });
+          resolve(stdout === output);
+        });
 
-    child.stdin.setEncoding('utf-8');
-    // This option is helpful for debugging:
-    // child.stdout.pipe(process.stdout);
-    child.stdin.write(input);
-    child.stdin.end();
+        child.stdin.setEncoding('utf-8');
+        // This option is helpful for debugging:
+        // child.stdout.pipe(process.stdout);
+        child.stdin.write(input);
+        child.stdin.end();
+      })
+      .catch((err) => reject(err));
   });
