e6788e9a9a4514f9ac3ea2816cfc3679ea2972b6
Dipendra Subedi <dipesube@gmail.com>
2022-11-20 18:12:35 -0500

    fixed syntax highlighting

--- src/frontend/src/app/problem/code-editor/code-editor.component.ts
+++ src/frontend/src/app/problem/code-editor/code-editor.component.ts
@@ -17,7 +17,8 @@ export class CodeEditorComponent {
   theme = 'vs-dark';
 
   codeModel: CodeModel = {
-    language: 'js',
+    language: 'javascript',
+    fileType: 'js',
     uri: 'solution.js',
     value: "import readline from 'readline'; const stdin = readline.createInterface({ input: process.stdin, output: process.stdout, }); stdin.question('', (input) => { const x = input.split(' '); const target = parseInt(x[1]); const nums = x[0].split(',').map(Number); const map = new Map(); let result = []; for (let i = 0; i < nums.length; i++) { const current = nums[i]; const match = map.get(target - current); if (match !== undefined) { result = [i, match]; break; } map.set(current, i); } console.log(result); stdin.close(); });"
   };
@@ -46,7 +47,7 @@ export class CodeEditorComponent {
   }
 
   submitCode() {
-    this.submitSolutionService.submitSolution(this.id, this.solution, this.codeModel.language).subscribe(
+    this.submitSolutionService.submitSolution(this.id, this.solution, this.codeModel.fileType).subscribe(
       (data) => {
         this.dialog.open(SubmissionDialog, {
           data: data.result
