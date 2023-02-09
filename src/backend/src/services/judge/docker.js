c8030d58f06d70d22c853b43f7a8c118750dc3cc
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-08 19:26:54 -0500

    Export fns for setting and cleaning up docker env

--- src/backend/src/services/judge/docker.js
+++ src/backend/src/services/judge/docker.js
@@ -1,6 +1,11 @@
+import { exec } from 'child_process';
 import { promises as fs } from 'fs';
+import util from 'util';
 
 import { languageImages, languageCommands } from './languages.js';
+import { generatePath } from './path-generator.js';
+
+const execAsync = util.promisify(exec);
 
 const dockerfileContents = (image, fileName, cmd) => `FROM ${image}
 ADD ${fileName} ${fileName}
@@ -24,4 +29,17 @@ const writeDockerfile = async (dockerPath, codePath, language) => {
   );
 };
 
-export { dockerRunCmd };
+const setupEnvironment = async (code, language) => {
+  const { id, codePath, dockerPath } = generatePath(language);
+  await fs.writeFile(codePath, code);
+  await writeDockerfile(dockerPath, codePath, language);
+  await execAsync(dockerBuildCmd(dockerPath, id));
+  return { id, codePath, dockerPath };
+};
+
+const cleanUpEnvironment = async ({ codePath, dockerPath }) => {
+  await fs.unlink(dockerPath);
+  await fs.unlink(codePath);
+};
+
+export { setupEnvironment, cleanUpEnvironment, dockerRunCmd };
