eb0b58e80bbc42fbc085c21fa5d58017af9d437c
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-08 19:23:31 -0500

    Add languages.js

---
+++ src/backend/src/services/judge/languages.js
@@ -0,0 +1,13 @@
+const languages = Object.freeze({
+  JAVASCRIPT: `mjs`,
+});
+
+const languageImages = Object.freeze({
+  mjs: `node`,
+});
+
+const languageCommands = Object.freeze({
+  mjs: `node`,
+});
+
+export { languages, languageImages, languageCommands };
