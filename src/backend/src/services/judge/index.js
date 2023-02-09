253166c2fb771b7afdbac5884ff2c7505f4d2deb
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-08 19:27:13 -0500

    Rewrite judge service to use docker commands

--- src/backend/src/services/judge/index.js
+++ src/backend/src/services/judge/index.js
@@ -1,11 +1,13 @@
 import { exec } from 'child_process';
-import { writeFile, unlink, access, constants, mkdir } from 'fs';
+import { access, constants, mkdir } from 'fs';
 
 import {
-  JUDGE_DIR,
-  generateCommand,
-  generatePath,
-} from './language-generators.js';
+  setupEnvironment,
+  cleanUpEnvironment,
+  dockerRunCmd,
+} from './docker.js';
+
+import { JUDGE_DIR } from './path-generator.js';
 
 const ensureJudgeDirExists = () =>
   new Promise((resolve, reject) => {
@@ -22,46 +24,41 @@ const ensureJudgeDirExists = () =>
     });
   });
 
-export default ({ language, code, input, output, timeout }) =>
+export default ({ language, code, input, output, timeLimit, memoryLimit }) =>
   new Promise((resolve, reject) => {
     ensureJudgeDirExists()
       .then(() => {
-        const codePath = generatePath(language);
-        writeFile(codePath, code, (err) => {
-          if (err) {
-            reject(err);
-          }
-        });
+        setupEnvironment(code, language)
+          .then(({ id, codePath, dockerPath }) => {
+            const cmd = dockerRunCmd(id, id, memoryLimit);
+            const child = exec(
+              cmd,
+              { timeout: timeLimit },
+              (err, _, stderr) => {
+                if (stderr) {
+                  reject(stderr);
+                }
+                if (err) {
+                  reject(err);
+                }
+              }
+            );
 
-        const cmd = generateCommand(language, codePath);
-        if (!cmd) {
-          reject(new Error(`Unsupported Language: ${language}`));
-        }
+            child.stdout.on('data', (stdout) => {
+              cleanUpEnvironment({ codePath, dockerPath })
+                .then(() => {
+                  resolve(stdout === output);
+                })
+                .catch((err) => reject(err));
+            });
 
-        const child = exec(cmd, { timeout }, (err, stdout, stderr) => {
-          if (stderr) {
-            reject(stderr);
-          }
-          if (err) {
-            reject(err);
-          }
-          if (stdout) {
-            resolve(stdout === output);
-          }
-        });
-
-        child.stdout.on('data', (stdout) => {
-          unlink(codePath, (err) => {
-            reject(err);
-          });
-          resolve(stdout === output);
-        });
-
-        child.stdin.setEncoding('utf-8');
-        // This option is helpful for debugging:
-        // child.stdout.pipe(process.stdout);
-        child.stdin.write(input);
-        child.stdin.end();
+            child.stdin.setEncoding('utf-8');
+            // This option is helpful for debugging:
+            // child.stdout.pipe(process.stdout);
+            child.stdin.write(input);
+            child.stdin.end();
+          })
+          .catch((err) => reject(err));
       })
       .catch((err) => reject(err));
   });
