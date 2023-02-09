bd78304f5215426b67ec83d31551a023a37d17d9
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-08 19:25:57 -0500

    Add function for writing docker file

--- src/backend/src/services/judge/docker.js
+++ src/backend/src/services/judge/docker.js
@@ -1,3 +1,7 @@
+import { promises as fs } from 'fs';
+
+import { languageImages, languageCommands } from './languages.js';
+
 const dockerfileContents = (image, fileName, cmd) => `FROM ${image}
 ADD ${fileName} ${fileName}
 CMD [ "${cmd}", "${fileName}" ]
@@ -8,5 +12,16 @@ const dockerBuildCmd = (dockerPath, tag) =>
 
 const dockerRunCmd = (imageName, id, memoryLimit) =>
   `docker run --rm --network none -m ${memoryLimit}M -i --name ${id} ${imageName}`;
+
+const writeDockerfile = async (dockerPath, codePath, language) => {
+  await fs.writeFile(
+    dockerPath,
+    dockerfileContents(
+      languageImages[language],
+      codePath.replace(/\\/g, '/'),
+      languageCommands[language]
+    )
+  );
+};
 
 export { dockerRunCmd };
