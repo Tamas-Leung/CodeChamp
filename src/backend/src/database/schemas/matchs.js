97701957d4dc7bf696abe75f5143799e4db00bdb
tamas-leung <tamasfun@gmail.com>
2023-02-10 19:34:04 -0500

    added matches

---
+++ src/backend/src/database/schemas/matchs.js
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+
+const MatchSchema = new Schema({
+    game_id: {
+        type: String,
+        required: true,
+    },
+    player_id: {
+        type: String,
+        required: true,
+    },
+    win: {
+        type: Boolean,
+        required: true,
+    },
+    rounds_completed: {
+        type: Number,
+        required: true,
+    },
+    problems: {
+        type: [
+            {
+                type: String,
+            },
+        ],
+        required: true,
+    },
+});
+
+export { MatchSchema };
---
+++ src/backend/src/services/matchs/index.js
@@ -0,0 +1,11 @@
+import { Matchs } from "../../database/mongoose.js";
+
+export const insertMatch = async (gameID, playerID, win, problemsPlayed) => {
+    await Matchs({
+        game_id: gameID,
+        player_id: playerID,
+        win: win,
+        rounds_completed: problemsPlayed.length - win ? 0 : 1,
+        problems: problemsPlayed
+    }).save();
+}
