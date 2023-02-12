06a9ee0591172fb8095066cff56cbe39fcf28c70
Anton Kanugalawattage <antondilon@gmail.com>
2023-02-12 07:01:24 -0500

    Format code results from submissions

--- src/frontend/src/app/problem/submission-list/submission-list.component.ts
+++ src/frontend/src/app/problem/submission-list/submission-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { SubmissionResult } from 'src/app/types/SubmissionResult';
-
+import * as prettier from 'prettier/standalone';
+import * as tsParser from 'prettier/parser-babel';
 @Component({
   selector: 'app-submission-list',
   templateUrl: './submission-list.component.html',
@@ -12,6 +13,19 @@ export class SubmissionListComponent {
 
   getSubmissionColor(submissionResult: SubmissionResult) {
     return submissionResult.correct ? 'success-text' : 'wrong-text';
+  }
+
+  getFormattedCode(submissionResult: SubmissionResult) {
+    console.log(
+      prettier.format(submissionResult.code, {
+        parser: 'babel',
+        plugins: [tsParser],
+      })
+    );
+    return prettier.format(submissionResult.code, {
+      parser: 'babel',
+      plugins: [tsParser],
+    });
   }
 
   constructor() {}
