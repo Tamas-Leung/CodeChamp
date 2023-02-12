9fcd19867bb6058abe5827c9be761038db854b15
Zhiming Zhao <zzmlegend@gmail.com>
2023-02-12 18:27:19 -0500

    Updated color for highlighted text

--- src/frontend/src/app/myprofile/myprofile.component.ts
+++ src/frontend/src/app/myprofile/myprofile.component.ts
@@ -13,7 +13,12 @@ import { ProblemsService } from '../services/problems/problems.service';
 export class MyProfileComponent {
   player: PlayerData;
   userStats: Stats[] | undefined;
-  constructor(private router: Router, private authService: AuthService, private profileService: ProfileService, private problemService: ProblemsService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private profileService: ProfileService,
+    private problemService: ProblemsService
+  ) {
     this.player = {
       id: 'a',
       name: '',
@@ -33,13 +38,15 @@ export class MyProfileComponent {
       this.userStats = res;
       let won = 0;
       let problemSolved = 0;
-      this.userStats.forEach(element => {
-        if (element.win) {won++;}
+      this.userStats.forEach((element) => {
+        if (element.win) {
+          won++;
+        }
         problemSolved += element.problems.length;
       });
-      const tempPlayerStats = this.player
+      const tempPlayerStats = this.player;
       tempPlayerStats.wins = won;
-      tempPlayerStats.losses = this.userStats.length-won;
+      tempPlayerStats.losses = this.userStats.length - won;
       tempPlayerStats.problemSolved = problemSolved;
       this.player = tempPlayerStats;
       console.log(this.userStats.length);
