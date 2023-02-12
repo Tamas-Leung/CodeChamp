43b298f1679a9284bd4847091270a203c77d51a6
Anton Kanugalawattage <antondilon@gmail.com>
2023-02-12 16:43:50 -0500

    Add prod urls

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   googleClientId:
     '1049277800628-djvnt32n0odf07kqelkn1t0iee33kk0n.apps.googleusercontent.com',
-  apiUrl: 'http://my-prod-url.com', // TODO: Update
-  wsUrl: 'ws://my-prod-url/ws', // TODO: Update
+  apiUrl: 'https://167.99.177.152:3000',
+  wsUrl: 'wss://167.99.177.152:7070/ws',
 };
