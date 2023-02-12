8e1ecb4e0728e4dc7e6bd2c081ceea826dba74cb
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-11 19:38:03 -0500

    Add judgeVerdict enum

---
+++ src/backend/src/services/judge/judge-verdict.js
@@ -0,0 +1,10 @@
+const judgeVerdict = Object.freeze({
+  CA: `Correct Answer`,
+  WA: 'Wrong Answer',
+  TLE: 'Time Limit Exceeded',
+  MLE: 'Memory Limit Exceeded',
+  CE: 'Code Error',
+  SE: 'Server Error',
+});
+
+export default judgeVerdict;
