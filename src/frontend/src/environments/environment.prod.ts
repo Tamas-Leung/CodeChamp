7c15d39f47a4bb057d4b8736b0a99340e51cb9e3
Anton Kanugalawattage <antondilon@gmail.com>
2023-02-12 15:33:38 -0500

    Create env variables to abstract prod & dev envs

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  googleClientId:
+    '1049277800628-djvnt32n0odf07kqelkn1t0iee33kk0n.apps.googleusercontent.com',
+  apiUrl: 'http://my-prod-url.com', // TODO: Update
+  wsUrl: 'ws://my-prod-url/ws', // TODO: Update
 };
