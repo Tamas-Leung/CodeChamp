f99dc47ec8111a265ee02c3a79fca1758c6c1d12
Tamas Leung <Tamasfun@gmail.com>
2023-02-12 23:41:12 -0500

    ws fix

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   googleClientId:
     '1049277800628-djvnt32n0odf07kqelkn1t0iee33kk0n.apps.googleusercontent.com',
   apiUrl: 'https://167.99.177.152:3000',
-  wsUrl: 'wss://167.99.177.152:7070/ws',
+  wsUrl: 'ws://167.99.177.152:7070/ws',
 };
