746f977618065c1a0bd2c8939cb4bc83e697c580
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-20 18:46:34 -0500

    Add backend python support

--- src/backend/src/services/judge/languages.js
+++ src/backend/src/services/judge/languages.js
@@ -1,13 +1,16 @@
 const languages = Object.freeze({
   JAVASCRIPT: `mjs`,
+  PYTHON: `py`,
 });
 
 const languageImages = Object.freeze({
   mjs: `node:19-alpine`,
+  py: 'python:3.11-alpine',
 });
 
 const languageCommands = Object.freeze({
   mjs: `node`,
+  py: `python`,
 });
 
 const languageIsSupported = (language) =>
