c94fe8c745efaaecb9ea772c5aaaeeddaa22c966
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-20 18:54:58 -0500

    Add language interface to frontend

---
+++ src/frontend/src/app/types/Language.ts
@@ -0,0 +1,9 @@
+export interface Language {
+    displayName: string;
+    extension: string,
+};
+
+const javaScript: Language = {displayName: 'JavaScript', extension: 'mjs'};
+const python: Language = {displayName: 'Python', extension: 'py'};
+
+export const languages = {javaScript, python};
