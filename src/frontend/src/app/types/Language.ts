cbbe48201eec5d160e6f133fb180915d8d1fd3ad
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-02-20 19:16:44 -0500

    Prettier Language.ts

--- src/frontend/src/app/types/Language.ts
+++ src/frontend/src/app/types/Language.ts
@@ -1,9 +1,9 @@
 export interface Language {
-    displayName: string;
-    extension: string,
-};
+  displayName: string;
+  extension: string;
+}
 
-const javaScript: Language = {displayName: 'JavaScript', extension: 'mjs'};
-const python: Language = {displayName: 'Python', extension: 'py'};
+const javaScript: Language = { displayName: 'JavaScript', extension: 'mjs' };
+const python: Language = { displayName: 'Python', extension: 'py' };
 
-export const languages = {javaScript, python};
+export const languages = { javaScript, python };
