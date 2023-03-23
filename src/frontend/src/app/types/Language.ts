9b05f4e1c18bd23857fa9b28e7936f8707f25490
Youssef Rizkalla <youssef.rizkalla1@gmail.com>
2023-03-23 18:11:03 -0400

    Add formatter for each language

--- src/frontend/src/app/types/Language.ts
+++ src/frontend/src/app/types/Language.ts
@@ -1,14 +1,11 @@
-import { CodeModel } from '@ngstack/code-editor';
 import * as prettier from 'prettier/standalone';
 import * as tsParser from 'prettier/parser-babel';
+
 export interface Language {
   displayName: string;
   extension: string;
   templateCode: string;
-  // The displayed code will not be reset unless it's completely re-assigned.
-  // So we need to reset the value after formatting by stringifying and parsing the object.
-  // https://github.com/ngstack/code-editor/issues/1030
-  format(codeModel: CodeModel): CodeModel;
+  format(code: string): string;
 }
 
 const javaScript: Language = {
@@ -16,13 +13,11 @@ const javaScript: Language = {
   extension: 'mjs',
   templateCode:
     "import readline from 'readline'; const stdin = readline.createInterface({ input: process.stdin, output: process.stdout, }); stdin.question('', (input) => { console.log(\"Your answer here...\"); stdin.close(); });",
-  format: (codeModel) => {
-    codeModel.value = prettier.format(codeModel.value, {
+  format: (code) =>
+    prettier.format(code, {
       parser: 'babel',
       plugins: [tsParser],
-    });
-    return JSON.parse(JSON.stringify(codeModel));
-  },
+    }),
 };
 
 const python: Language = {
@@ -30,7 +25,7 @@ const python: Language = {
   extension: 'py',
   templateCode: `inp = input()
 print('Your answer here...')`,
-  format: (codeModel) => JSON.parse(JSON.stringify(codeModel)),
+  format: (code) => code,
 };
 
 export const languages = { javaScript, python };
